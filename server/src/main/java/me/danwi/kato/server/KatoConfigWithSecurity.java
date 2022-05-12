209af4ee9dc466e94b3c55f5aaf8a0ab4f2497d6
李雨田 <demon@danwi.me>
2022-05-12 14:22:11 +0800

    修改ConditionalOnClass的表达

--- server/src/main/java/me/danwi/kato/server/KatoConfigWithSecurity.java
+++ server/src/main/java/me/danwi/kato/server/KatoConfigWithSecurity.java
@@ -3,14 +3,15 @@ package me.danwi.kato.server;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 
 /**
  * 当Security被引入的时候,配置能处理AccessDeniedException等异常的ResponseBodyAdvice
  */
 @Configuration("me.danwi.kato.ConfigWithSecurity")
-@ConditionalOnClass({AccessDeniedException.class, AuthenticationException.class})
+@ConditionalOnClass(name = {
+        "org.springframework.security.access.AccessDeniedException",
+        "org.springframework.security.core.AuthenticationException",
+})
 public class KatoConfigWithSecurity {
     @Bean("me.danwi.kato.ResponseBodyAdviceWithSecurity")
     KatoResponseBodyAdviceWithSecurity exceptionHandler() {
