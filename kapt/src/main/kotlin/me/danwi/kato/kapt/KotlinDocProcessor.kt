f4fe3a434c41a39f7681dbf9a1501dec3c2ac536
李雨田 <demon@danwi.me>
2022-05-17 00:36:04 +0800

    添加class声明遍历结构

--- kapt/src/main/kotlin/me/danwi/kato/kapt/KotlinDocProcessor.kt
+++ kapt/src/main/kotlin/me/danwi/kato/kapt/KotlinDocProcessor.kt
@@ -1,11 +1,57 @@
 package me.danwi.kato.kapt
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.devtools.ksp.closestClassDeclaration
 import com.google.devtools.ksp.processing.*
-import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.*
+import com.google.devtools.ksp.visitor.KSTopDownVisitor
+import me.danwi.kato.apt.model.ClassDoc
 
 class KotlinDocProcessor(private val codeGenerator: CodeGenerator) : SymbolProcessor {
+    private val mapper = ObjectMapper()
+
+    private val KSClassDeclaration.javaClassName: String
+        get() {
+            val parentClass = parentDeclaration?.closestClassDeclaration()
+            return if (parentClass == null)
+                simpleName.asString()
+            else
+                "${parentClass.javaClassName}\$${simpleName.asString()}"
+
+        }
+
+
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        return listOf()
+        resolver.getAllFiles().forEach {
+            it.accept(object : KSTopDownVisitor<Unit, Unit>() {
+                override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
+                    super.visitClassDeclaration(classDeclaration, data)
+                    val sourceFile = classDeclaration.containingFile ?: return
+                    try {
+                        val kotlinDocFile = codeGenerator.createNewFile(
+                            Dependencies(false, sourceFile),
+                            classDeclaration.packageName.asString(),
+                            classDeclaration.javaClassName,
+                            "javadoc.json"
+                        )
+                        kotlinDocFile.use { s ->
+                            val classDoc = generateClassDoc(classDeclaration)
+                            s.write(mapper.writeValueAsBytes(classDoc))
+                        }
+                    } catch (_: Exception) {
+                    }
+                }
+
+                override fun defaultHandler(node: KSNode, data: Unit) {}
+            }, Unit)
+        }
+        return emptyList()
+    }
+
+    fun generateClassDoc(classDeclaration: KSClassDeclaration): ClassDoc {
+        val classDoc = ClassDoc()
+        classDoc.description = classDeclaration.docString
+        return classDoc
     }
 }
 
