a9816ba21eb87667b41b9fbd91f487d1d934e031
WJY <wjy.jy@foxmail.com>
2022-06-28 13:47:50 +0800

    KatoUndeclaredException 增加构造函数，在Client时会调用单String构造函数实例化

--- common/src/main/java/me/danwi/kato/common/exception/KatoUndeclaredException.java
+++ common/src/main/java/me/danwi/kato/common/exception/KatoUndeclaredException.java
@@ -4,6 +4,14 @@ package me.danwi.kato.common.exception;
  * 未被业务代码处理的异常,通常出现这种异常需要引起开发人员的注意
  */
 public class KatoUndeclaredException extends KatoException {
+    public KatoUndeclaredException(String message) {
+        super(message);
+    }
+
+    public KatoUndeclaredException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public KatoUndeclaredException(Throwable cause) {
         super(cause);
     }
