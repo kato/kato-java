dee58c5912875bf4247b85b5f7c63fd30ed19e4d
WJY <wjy.jy@foxmail.com>
2022-07-01 15:08:12 +0800

    解决当response是空时，json反序列化错误问题； com.fasterxml.jackson.databind.exc.MismatchedInputException: No content to map due to end-of-input  at [Source: (String)""; line: 1, column: 0]

--- client/src/main/java/me/danwi/kato/client/KatoResultDecoder.java
+++ client/src/main/java/me/danwi/kato/client/KatoResultDecoder.java
@@ -20,6 +20,7 @@ public class KatoResultDecoder implements Decoder {
             return null;
         //反序列化结果
         String bodyStr = Util.toString(response.body().asReader(Util.UTF_8));
+        if (Util.isBlank()) return null
         return mapper.readValue(bodyStr, TypeFactory.defaultInstance().constructType(type));
     }
 }
