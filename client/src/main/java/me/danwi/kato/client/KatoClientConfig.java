d943a87388c00cd04c28a84f564df8907cb21118
王加雨 <wjy.jy@foxmail.com>
2022-07-08 17:12:21 +0800

    删除无用的自定义Decoder

--- client/src/main/java/me/danwi/kato/client/KatoClientConfig.java
+++ client/src/main/java/me/danwi/kato/client/KatoClientConfig.java
@@ -1,16 +1,11 @@
 package me.danwi.kato.client;
 
-import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration("me.danwi.kato.client.Config")
 public class KatoClientConfig {
-    @Bean("me.danwi.kato.client.ResultDecoder")
-    Decoder resultDecoder() {
-        return new KatoResultDecoder();
-    }
 
     @Bean("me.danwi.kato.client.ErrorDecoder")
     ErrorDecoder errorDecoder() {
