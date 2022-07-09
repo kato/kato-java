c90f44d60c9eb0def5b468fd9c232e429ff4a381
王加雨 <wangjiayu@bjknrt.com>
2022-07-09 09:44:17 +0800

    解决javadoc构建警告

--- common/src/main/java/me/danwi/kato/common/argument/MultiRequestBody.java
+++ common/src/main/java/me/danwi/kato/common/argument/MultiRequestBody.java
@@ -12,17 +12,20 @@ public @interface MultiRequestBody {
 
     /**
      * 设置解析key
+     *
+     * @return 解析key
      */
     String value() default "";
 
     /**
-     * 是否必填参数
+     * @return 是否必填参数
      */
     boolean required() default true;
 
     /**
      * 当body不是JSONObject或者不能匹配到key时
-     * 是否将整个body作为参数解析
+     *
+     * @return 是否将整个body作为参数解析
      */
     boolean parseBodyIfMissKey() default true;
 
