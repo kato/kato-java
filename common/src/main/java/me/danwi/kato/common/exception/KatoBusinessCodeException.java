ff4eefa6b07d8383d90ef573fb29ba3342e88d68
王加雨 <wjy.jy@foxmail.com>
2023-03-11 17:21:59 +0800

    BusinessCode异常code值返回

--- common/src/main/java/me/danwi/kato/common/exception/KatoBusinessCodeException.java
+++ common/src/main/java/me/danwi/kato/common/exception/KatoBusinessCodeException.java
@@ -1,10 +1,14 @@
 package me.danwi.kato.common.exception;
 
+import java.util.Map;
+import java.util.Optional;
+
 /**
  * 根据code定义不同逻辑处理异常
  */
-public class KatoBusinessCodeException extends KatoBusinessException {
-    private final int code;
+public class KatoBusinessCodeException extends KatoBusinessException implements ExceptionExtraDataHolder {
+    public static final String CODE_KEY = "code";
+    private Integer code;
 
     public KatoBusinessCodeException(int code, String message) {
         super(message);
@@ -14,4 +18,16 @@ public class KatoBusinessCodeException extends KatoBusinessException {
     public int getCode() {
         return code;
     }
+
+    @Override
+    public Map<String, Object> toMap() {
+        return Map.of(CODE_KEY, code);
+    }
+
+    @Override
+    public void loadFromMap(Map<String, Object> map) {
+        if (map != null)
+            Optional.ofNullable((String) map.get(CODE_KEY)).ifPresent(it -> this.code = Integer.valueOf(it));
+    }
+
 }
